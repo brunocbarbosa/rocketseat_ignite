77dbd675576142d29911dcb276200181fce9918f
Bruno Barbosa <bruno.cbarbosa90@gmail.com>
2023-06-02 10:37:56 -0300

    project01, part 4 finished

--- nodejs/project01/src/server.js
+++ nodejs/project01/src/server.js
@@ -1,5 +1,9 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
 import { json } from './middlewares/json.js'
+import { Database } from './database.js'
+
+const databasae = new Database()
 
 const users = []
 
@@ -9,18 +13,21 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   if(method === 'GET' && url === '/users'){
-    return res
-    .end(JSON.stringify(users))
+    const users = databasae.select('users')
+
+    return res.end(JSON.stringify(users))
   }
 
   if(method === 'POST' && url === '/users'){
     const {name, email} = req.body
 
-    users.push({
-      id: 1,
+    const user = {
+      id: randomUUID(),
       name,
       email
-    })
+    }
+
+    databasae.insert('users', user)
 
     return res.writeHead(201).end()
   }
