655fa4b140bd4421ca56b04bd93d44b3ad46b845
Bruno Barbosa <bruno.cbarbosa90@gmail.com>
2023-06-14 01:19:22 -0300

    project02 part 5

--- nodejs/2/project/src/env/index.ts
+++ nodejs/2/project/src/env/index.ts
@@ -1,10 +1,7 @@
 import { config } from 'dotenv'
 import { z } from 'zod'
 
-console.log(process.env.NODE_ENV)
-
 if (process.env.NODE_ENV === 'test') {
-  console.log('entrou')
   config({ path: '.env.test' })
 } else {
   config()
