ac5abd85391498c7faf5fdf5c536f6cab77b8fd6
Bruno Barbosa <bruno.cbarbosa90@gmail.com>
2023-08-09 15:05:27 -0300

    created check-ins routes

--- nodejs/3/project/src/use-cases/factories/make-validate-check-in-use-case.ts
+++ nodejs/3/project/src/use-cases/factories/make-validate-check-in-use-case.ts
@@ -1,7 +1,7 @@
 import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
 import { ValidateCheckInUseCase } from '../validate-check-in'
 
-export function makeFetchUserCheckInHistoryUseCase() {
+export function makeValidateCheckInUseCase() {
   const prismaCheckInRepository = new PrismaCheckInsRepository()
   const useCase = new ValidateCheckInUseCase(prismaCheckInRepository)
 
